a8553369281a33dae6de43bcf9cad8bb0ead6d45
Davi Guimarães <ddaavvii2001@gmail.com>
2025-07-17 22:59:19 -0300

    feat. metricas second solution

--- worker/src/main/java/br/com/solarz/worker/service/RedisQueueService_SecondSolution.java
+++ worker/src/main/java/br/com/solarz/worker/service/RedisQueueService_SecondSolution.java
@@ -1,11 +1,13 @@
 package br.com.solarz.worker.service;
 
 import br.com.solarz.worker.config.RedisClientProvider;
+import br.com.solarz.worker.model.Api;
 import br.com.solarz.worker.model.ApiScore;
 import br.com.solarz.worker.model.Usina;
 import br.com.solarz.worker.repository.ApiRepository;
 import br.com.solarz.worker.repository.ApiScoreRepository;
 import br.com.solarz.worker.repository.UsinaRepository;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -27,31 +29,64 @@ public class RedisQueueService_SecondSolution {
     private final ApiRepository apiRepository;
     private final MeterRegistry meterRegistry;
 
-    Map<Long, Double> apiErrorRateScores = new HashMap<>();
+    RScoredSortedSet<Long> queue;
+    Map<Long, Double> availableScores = new HashMap<>();
+    Map<Long, Double> errorScores = new HashMap<>();
 
     @PostConstruct
     public void setup() {
-        this.redissonClient = redisClientProvider.getClient();
-        List<ApiScore> scores = apiScoreRepository.findAll();
-        scores.sort(Comparator.comparingDouble(ApiScore::getErrorRate));
+        redissonClient = redisClientProvider.getClient();
+        queue = redissonClient.getScoredSortedSet("queue_second_solution");
 
-        for (int i = 0; i < scores.size(); i++) {
-            ApiScore score = scores.get(i);
-            apiErrorRateScores.put(score.getId(), (double) i);
+        List<ApiScore> apiScores = apiScoreRepository.findAll();
+
+        apiScores.sort(Comparator.comparingDouble(ApiScore::calculate));
+        for (int i = 0; i < apiScores.size(); i++) {
+            ApiScore apiScore = apiScores.get(i);
+            Api api = apiScore.getApi();
+
+            double scoreHigh = i;
+            double scoreNormal = i + apiScores.size();
+
+            availableScores.put(apiScore.getId(), scoreHigh);
+
+            Gauge.builder("available.queue.size", queue, q -> q.count(scoreHigh, true, scoreHigh, true))
+                    .tags("portal", api.getName(), "priority", "HIGH")
+                    .register(meterRegistry);
+
+            Gauge.builder("available.queue.size", queue, q -> q.count(scoreNormal, true, scoreNormal, true))
+                    .tags("portal", api.getName(), "priority", "NORMAL")
+                    .register(meterRegistry);
+        }
+
+        apiScores.sort(Comparator.comparingDouble(ApiScore::getErrorRate));
+        for (int i = 0; i < apiScores.size(); i++) {
+            ApiScore apiScore = apiScores.get(i);
+            Api api = apiScore.getApi();
+
+            double scoreHigh = i + apiScores.size() * 2;
+            double scoreNormal = i + apiScores.size() * 3;
+
+            errorScores.put(apiScore.getId(), (double) i);
+
+            Gauge.builder("error.queue.size", queue, q -> q.count(scoreHigh, true, scoreHigh, true))
+                    .tags("portal", api.getName(), "priority", "HIGH")
+                    .register(meterRegistry);
+
+            Gauge.builder("error.queue.size", queue, q -> q.count(scoreNormal, true, scoreNormal, true))
+                    .tags("portal", api.getName(), "priority", "NORMAL")
+                    .register(meterRegistry);
         }
     }
 
     public synchronized List<Usina> dequeue(int amount) {
-        RScoredSortedSet<Long> queue =  redissonClient.getScoredSortedSet("queue_second_solution");
         return usinaRepository.findAllById(queue.pollFirst(amount));
     }
 
     public void queueFailed(Usina usina) {
-        RScoredSortedSet<Long> queue =  redissonClient.getScoredSortedSet("queue_second_solution");
-
         long apiId = usina.getCredencial().getApi().getId();
-        int offset = apiErrorRateScores.size() * 2;
-        double score = apiErrorRateScores.get(apiId) + offset;
+        int offset = errorScores.size() * (usina.getPriority().equals(Usina.Priority.HIGH) ? 2 : 3);
+        double score = errorScores.get(apiId) + offset;
         queue.add(score, usina.getId());
     }
 }
