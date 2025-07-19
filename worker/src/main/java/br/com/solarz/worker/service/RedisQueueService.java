fe24ee3717e7a5f305308569ba1832cca90810b6
Davi Guimarães <ddaavvii2001@gmail.com>
2025-07-19 01:42:59 -0300

    Primeira solução com fila única

--- worker/src/main/java/br/com/solarz/worker/service/RedisQueueService.java
+++ worker/src/main/java/br/com/solarz/worker/service/RedisQueueService.java
@@ -2,15 +2,15 @@ package br.com.solarz.worker.service;
 
 import config.RedisClientProvider;
 import model.Api;
+import model.ApiScore;
 import model.Usina;
-import model.Usina.Priority;
-import repository.ApiRepository;
+import org.redisson.api.RScoredSortedSet;
+import repository.ApiScoreRepository;
 import repository.UsinaRepository;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.redisson.api.RSet;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
@@ -20,115 +20,74 @@ import java.util.*;
 @RequiredArgsConstructor
 public class RedisQueueService {
 
-    public enum QueueType {
-        AVAILABLE, // fila usinas disponíveis para atualização
-        FAILED, // fila usinas que falharam a atualização
-        AVAILABLE_OR_FAILED //usado para pegar dados das duas filas ao mesmo tempo, priorizando a fila available
-    };
-
-    private final HashMap<String, HashMap<Integer, RSet<Long>>> queues = new HashMap<>();
     private RedissonClient redissonClient;
 
     private final RedisClientProvider redisClientProvider;
+    private final ApiScoreRepository apiScoreRepository;
     private final UsinaRepository usinaRepository;
-    private final ApiRepository apiRepository;
     private final MeterRegistry meterRegistry;
+
+    RScoredSortedSet<Long> queue;
+    Map<Long, Double> availableScores = new HashMap<>();
+    Map<Long, Double> errorScores = new HashMap<>();
 
     @PostConstruct
     public void setup() {
-        this.redissonClient = redisClientProvider.getClient();
-        setupQueues();
+        redissonClient = redisClientProvider.getClient();
+        queue = redissonClient.getScoredSortedSet("usinas_queue");
+
+        buildMeters();
     }
 
-    public void setupQueues() {
-        List<Api> apis = apiRepository.findAll();
-        for (Api api : apis) {
-            String avaQueueName = buildName(api, QueueType.AVAILABLE);
-            HashMap<Integer, RSet<Long>> available = new HashMap<>();
-            available.put(Priority.HIGH.ordinal(), redissonClient.getSet(avaQueueName + "_" + Priority.HIGH));
-            available.put(Priority.NORMAL.ordinal(), redissonClient.getSet(avaQueueName + "_" + Priority.NORMAL));
+    private void buildMeters() {
+        List<ApiScore> apiScores = apiScoreRepository.findAll();
 
-            String errQueueName = buildName(api, QueueType.FAILED);
-            HashMap<Integer, RSet<Long>> error = new HashMap<>();
-            error.put(Priority.HIGH.ordinal(), redissonClient.getSet(errQueueName + "_" + Priority.HIGH));
-            error.put(Priority.NORMAL.ordinal(), redissonClient.getSet(errQueueName + "_" + Priority.NORMAL));
+        apiScores.sort(Comparator.comparingDouble(ApiScore::calculate));
+        for (int i = 0; i < apiScores.size(); i++) {
+            ApiScore apiScore = apiScores.get(i);
+            Api api = apiScore.getApi();
 
-            queues.put(avaQueueName, available);
-            queues.put(errQueueName, error);
+            double scoreHigh = i;
+            double scoreNormal = i + apiScores.size();
 
-            buildMeters(api);
-        }
-    }
+            availableScores.put(apiScore.getId(), scoreHigh);
 
-    private void buildMeters(Api api) {
-        String avaQueueName = buildName(api, QueueType.AVAILABLE);
-        String errQueueName = buildName(api, QueueType.FAILED);
+            Gauge.builder("available.queue.size", queue, q -> q.count(scoreHigh, true, scoreHigh, true))
+                    .tags("portal", api.getName(), "priority", "HIGH")
+                    .register(meterRegistry);
 
-        for (var priority : queues.get(avaQueueName).keySet()) {
-            String priorityName = Priority.values()[priority].name();
-
-            Gauge.builder("available.queue.size", queues, queues -> queues.get(avaQueueName).get(priority).size())
-                    .tags("portal", api.getName(), "priority", priorityName)
+            Gauge.builder("available.queue.size", queue, q -> q.count(scoreNormal, true, scoreNormal, true))
+                    .tags("portal", api.getName(), "priority", "NORMAL")
                     .register(meterRegistry);
         }
 
-        for (var priority : queues.get(errQueueName).keySet()) {
-            String priorityName = Priority.values()[priority].name();
+        apiScores.sort(Comparator.comparingDouble(ApiScore::getErrorRate));
+        for (int i = 0; i < apiScores.size(); i++) {
+            ApiScore apiScore = apiScores.get(i);
+            Api api = apiScore.getApi();
 
-            Gauge.builder("error.queue.size", queues, queues -> queues.get(errQueueName).get(priority).size())
-                    .tags("portal", api.getName(), "priority", priorityName)
+            double scoreHigh = i + apiScores.size() * 2;
+            double scoreNormal = i + apiScores.size() * 3;
+
+            errorScores.put(apiScore.getId(), (double) i);
+
+            Gauge.builder("error.queue.size", queue, q -> q.count(scoreHigh, true, scoreHigh, true))
+                    .tags("portal", api.getName(), "priority", "HIGH")
+                    .register(meterRegistry);
+
+            Gauge.builder("error.queue.size", queue, q -> q.count(scoreNormal, true, scoreNormal, true))
+                    .tags("portal", api.getName(), "priority", "NORMAL")
                     .register(meterRegistry);
         }
     }
 
-    public synchronized Set<Usina> dequeue(Api api, QueueType type, int amount, Priority priority) {
-        String queueName = buildName(api, type);
-        Set<Usina> usinas = new HashSet<>();
-        var queueWithPriority = queues.get(queueName);
-
-        if (priority != null) {
-            RSet<Long> queue = queueWithPriority.get(priority.ordinal());
-            Set<Long> ids = queue.removeRandom(amount);
-            return new HashSet<>(usinaRepository.findAllById(ids));
-        }
-
-        for (RSet<Long> queue : queueWithPriority.values()) {
-            if (amount - usinas.size() > 0) {
-                Set<Long> ids = queue.removeRandom(amount - usinas.size());
-
-                if (!ids.isEmpty())
-                    usinas.addAll(usinaRepository.findAllById(ids));
-            }
-        }
-
-        return usinas;
+    public synchronized List<Usina> dequeue(int amount) {
+        return usinaRepository.findAllById(queue.pollFirst(amount));
     }
 
-    public Set<Usina> getUsinasByApi(Api api, QueueType type, int amount, Priority priority) {
-        if (type == QueueType.AVAILABLE_OR_FAILED) {
-            Set<Usina> usinas = dequeue(api, QueueType.AVAILABLE, amount, priority);
-            if (usinas.size() < amount) {
-                Set<Usina> failed = dequeue(api, QueueType.FAILED, amount - usinas.size(), priority);
-                usinas.addAll(failed);
-            }
-
-            return usinas;
-        }
-
-        return dequeue(api, type, amount, priority);
-    }
-
-    public void queueFailed(List<Usina> usinas, Api api) {
-        String errQueueName = buildName(api, QueueType.FAILED);
-        var apiQueues = queues.get(errQueueName);
-
-        for (Usina usina : usinas) {
-            var queue = apiQueues.get(usina.getPriority().ordinal());
-            queue.add(usina.getId());
-        }
-    }
-
-    public String buildName(Api api, QueueType type) {
-        return api.getName() + "_" + type.name();
+    public void queueFailed(Usina usina) {
+        long apiId = usina.getCredencial().getApi().getId();
+        double score = errorScores.get(apiId) + usina.getUpdateAttempts() - 1;
+        queue.add(score, usina.getId());
     }
 }
